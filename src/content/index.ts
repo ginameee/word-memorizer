afafa842a1912ab8fd53f4742d08057a0a3f1098
Jangchun Lee <adama@wemakeprice.com>
2020-07-09 16:23:28 +0900

    메세지 선택, 백그라운드 전송 구현

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,1 +1,9 @@
-console.log("hihi");
+document.addEventListener('mouseup', sendWord);
+
+function sendWord() {
+    const selected = window.getSelection()?.toString();
+
+    if (selected && selected.length > 0) {
+        chrome.runtime.sendMessage({ 'word': selected });
+    }
+}
