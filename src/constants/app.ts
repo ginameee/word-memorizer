d7db56d17b224efc971b278629cff0c5377ab9e2
Jangchun Lee <adama@wemakeprice.com>
2020-07-09 16:24:24 +0900

    환경설정 및 메뉴이름 수정

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,3 +1,5 @@
+import * as manifest from '@/manifest.json';
+
 interface IMenu {
     id: string;
     title: string;
@@ -5,9 +7,11 @@ interface IMenu {
     iconClassName?: string;
 }
 
-export const menuList: IMenu[] = [
-    { id: 'word', title: '나의단어', path: '/word', iconClassName: 'el-icon-edit' },
+export const MENU_LIST: IMenu[] = [
+    { id: 'word', title: '단어장', path: '/word', iconClassName: 'el-icon-edit' },
     { id: 'notification', title: '공지사항', path: '/notification', iconClassName: 'el-icon-bell' },
     { id: 'setting', title: '설정', path: '/setting', iconClassName: 'el-icon-setting' },
     { id: 'infomation', title: '정보', path: '/information', iconClassName: 'el-icon-info' },
 ];
+
+export const APP_NAME = manifest.name;
